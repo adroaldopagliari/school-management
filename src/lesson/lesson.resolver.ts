383a8de93cf69bf3c1690ff4f3fa08ffd866408f
Adroaldo J. Pagliari <adroaldo@outlook.com>
2020-11-29 18:02:27 -0300

    feat: add lesson service

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,10 +1,20 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Mutation, Query, Resolver } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
 
 @Resolver((of) => LessonType)
 export class LessonResolver {
   @Query((returns) => LessonType)
   lesson() {
+    return {
+      id: '1232342d-234fs23-sdfger23-23423fds2',
+      name: 'Math class',
+      startDate: new Date().toISOString(),
+      endDate: new Date().toISOString(),
+    };
+  }
+
+  @Mutation((returns) => LessonType)
+  createLesson() {
     return {
       id: '1232342d-234fs23-sdfger23-23423fds2',
       name: 'Math class',
