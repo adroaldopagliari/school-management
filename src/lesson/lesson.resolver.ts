af06c2fda4e29c9c89aea007d78c357eb1e00bc0
Adroaldo J. Pagliari <adroaldo@outlook.com>
2020-11-29 18:11:48 -0300

    feat: add create lesson mutation

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,8 +1,11 @@
-import { Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { LessonService } from './lesson.service';
 import { LessonType } from './lesson.type';
 
 @Resolver((of) => LessonType)
 export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+
   @Query((returns) => LessonType)
   lesson() {
     return {
@@ -14,12 +17,11 @@ export class LessonResolver {
   }
 
   @Mutation((returns) => LessonType)
-  createLesson() {
-    return {
-      id: '1232342d-234fs23-sdfger23-23423fds2',
-      name: 'Math class',
-      startDate: new Date().toISOString(),
-      endDate: new Date().toISOString(),
-    };
+  createLesson(
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string,
+  ) {
+    return this.lessonService.createLesson(name, startDate, endDate);
   }
 }
