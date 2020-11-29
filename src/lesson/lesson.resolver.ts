55500e3cea77aee4975498b81bb2b37bc2987339
Adroaldo J. Pagliari <adroaldo@outlook.com>
2020-11-29 01:09:50 -0300

    feat: add lesson graphql resolver

---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,15 @@
+import { Query, Resolver } from '@nestjs/graphql';
+import { LessonType } from './lesson.type';
+
+@Resolver((of) => LessonType)
+export class LessonResolver {
+  @Query((returns) => LessonType)
+  lesson() {
+    return {
+      id: '1232342d-234fs23-sdfger23-23423fds2',
+      name: 'Math class',
+      startDate: new Date().toISOString(),
+      endDate: new Date().toISOString(),
+    };
+  }
+}
