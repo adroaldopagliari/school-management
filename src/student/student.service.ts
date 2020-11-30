f1a07865f13c678fd70a45dcd028ede6fd63b0d2
Adroaldo J. Pagliari <adroaldo@outlook.com>
2020-11-29 23:56:54 -0300

    feat: add student module

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { Student } from './student.entity';
+import { CreateStudentInput } from './create-student.input';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  constructor(
+    @InjectRepository(Student) private studentRepository: Repository<Student>,
+  ) {}
+
+  async getStudent(id: string): Promise<Student> {
+    const student = await this.studentRepository.findOne({ id });
+
+    if (!student) {
+    }
+
+    return student;
+  }
+
+  async getStudents(): Promise<Student[]> {
+    const students = await this.studentRepository.find();
+
+    if (!students) {
+    }
+
+    return students;
+  }
+
+  async createStudent(createLessonInput: CreateStudentInput): Promise<Student> {
+    const { firstName, lastName } = createLessonInput;
+
+    const student = this.studentRepository.create({
+      id: uuid(),
+      firstName,
+      lastName,
+    });
+
+    return this.studentRepository.save(student);
+  }
+}
