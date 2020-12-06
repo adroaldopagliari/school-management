edca233d72757b5dc06dac4bd4e41f643e2a08db
Adroaldo J. Pagliari <adroaldo@outlook.com>
2020-12-06 20:16:13 -0300

    feat: resolve students field in lesson

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,12 +1,24 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
 import { AssignStudentsToLessonInput } from './assign-students-to-lesson-input';
+import { Lesson } from './lesson.entity';
 import { CreateLessonInput } from './lesson.input';
 import { LessonService } from './lesson.service';
+import { StudentService } from '../student/student.service';
 import { LessonType } from './lesson.type';
 
 @Resolver((of) => LessonType)
 export class LessonResolver {
-  constructor(private lessonService: LessonService) {}
+  constructor(
+    private lessonService: LessonService,
+    private studentService: StudentService,
+  ) {}
 
   @Query((returns) => LessonType)
   lesson(@Args('id') id: string) {
@@ -33,5 +45,10 @@ export class LessonResolver {
     return this.lessonService.assingStudentsToLessons(
       assignStudentsToLessonInput,
     );
+  }
+
+  @ResolveField()
+  async students(@Parent() lesson: Lesson) {
+    return this.studentService.getManyStudents(lesson.students);
   }
 }
